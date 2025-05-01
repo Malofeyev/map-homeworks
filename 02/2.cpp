f32391819e4399a0c6dfb06b574e962ead47dcf7
imalofeev98 <malofeevivan98@mail.ru>
2025-05-01 16:22:18 +0300

    Changed 02/2.cpp

--- 02/2.cpp
+++ 02/2.cpp
@@ -3,6 +3,8 @@
 #include <atomic>
 #include <chrono>
 #include <thread>
+#include <mutex>
+#include <iomanip>
 #include <vector>
 
 #ifdef _WIN32
@@ -21,17 +23,50 @@ void clearConsole() {
     #endif
 }
 
-void work(std::atomic_int& work_cnt) {
-    while (work_cnt.load(std::memory_order::memory_order_acquire) > 0) {
-        work_cnt.fetch_sub(1, std::memory_order::memory_order_release);  
-        std::this_thread::sleep_for(1s);
+const int NUM_WIDTH = 2;
+const int ID_WIDTH = 20;
+const int NORMALIZED_PROGRESS_CNT = 20;
+const int TIME_WIDTH = 10;
+
+void setNextLine(int y) {
+    std::cout << "\033[" << y + 1 << ";" << NUM_WIDTH + ID_WIDTH + NORMALIZED_PROGRESS_CNT + TIME_WIDTH + 1 << "H" << '\n'; 
+    std::cout.flush();
+}
+
+void setProgressAtPosition(int left, int right, int y) {
+    for (int i = left; i <= right; i++) {
+        std::cout << "\033[" << y + 1 << ";" << i + NUM_WIDTH + ID_WIDTH << "H" << "\u2588";
     }
 }
 
+std::mutex write_lock;
 
-void print_head(int num, const std::thread& thread) {
-    std::cout << num << ") thread:" << thread.get_id() << '\n';
+void work(int num, int work_cnt) {
+    std::chrono::steady_clock::time_point start = 
+        std::chrono::steady_clock::now();
+    const auto idle = 500ms;
+    int left = 1;
+    for (int finished_cnt = 1; finished_cnt < work_cnt; finished_cnt++) {
+        int right = 
+            finished_cnt * NORMALIZED_PROGRESS_CNT / work_cnt;
+        std::this_thread::sleep_for(idle);
+        std::lock_guard<std::mutex> lock(write_lock);
+        setProgressAtPosition(left, right, num);
+        std::cout.flush();
+        left = right + 1;
+    }
+
+    int right = NORMALIZED_PROGRESS_CNT;
+    std::this_thread::sleep_for(idle);
+    std::lock_guard<std::mutex> lock(write_lock);
+    setProgressAtPosition(left, right, num);
+    std::chrono::steady_clock::time_point end = 
+        std::chrono::steady_clock::now();
+    std::chrono::duration<double> duration = end - start;
+    std::cout << std::setw(TIME_WIDTH) << duration.count(); 
+    std::cout.flush();
 }
+
 
 int main(int argc, char *argv[]) {
     int thread_cnt, work_cnt;
@@ -44,65 +79,32 @@ int main(int argc, char *argv[]) {
     int mod = work_cnt % thread_cnt;
 
     std::vector<std::thread> threads;
-    std::vector<int> work_cnts;
-    std::vector<std::atomic_int> cur_work_cnts(thread_cnt);
-    std::vector<std::chrono::steady_clock::time_point> starts;
-    std::vector<std::chrono::duration<double>> durations;
     threads.reserve(thread_cnt);
-    work_cnts.reserve(thread_cnt);
-    starts.reserve(thread_cnt);
-    durations.reserve(thread_cnt);
 
-    for (int i = 0; i < thread_cnt; i++) {
-        int work_per_thread = div + (i < mod?1:0);
-        work_cnts.push_back(work_per_thread);
-        cur_work_cnts.at(i).store(work_per_thread);
-        durations.emplace_back(0);
-    }
 
-    for (int i = 0; i < thread_cnt; i++) {
-        threads.emplace_back(work, std::ref(cur_work_cnts.at(i)));
-        starts.push_back(std::chrono::steady_clock::now());
-    }
-
-    bool is_finished = false;
-    int normalized_cnt = 20;
-    while (!is_finished) {
-        clearConsole();
-        is_finished = true;
-        for (int i = 0; i < thread_cnt; i++) {
-            print_head(i, threads[i]);
-            int cur_work = cur_work_cnts.at(i).load(std::memory_order_acquire);
-            if (cur_work > 0) {
-                int finished_cnt = work_cnts[i] - cur_work; 
-                int normalized_finished_cnt = finished_cnt * normalized_cnt / work_cnts[i];
-                std::cout << '[' << 
-                    std::string(normalized_finished_cnt, '#') << 
-                    std::string(normalized_cnt - normalized_finished_cnt, '.') << 
-                    "]\n";
-                is_finished = false;
-            } else {
-                if (durations[i].count() == 0) {
-                    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-                    durations[i] = end - starts[i];
-                }
-                std::cout << "Time: " << durations[i].count() << '\n';
-                
-            }
-        }
-
-        std::cout << std::endl;
-        std::this_thread::sleep_for(2s);
-    }
-
-    for (int i = 0; i < thread_cnt; i++) {
-        threads[i].join();
-    }
-
+    // # id Progress Bar Time
     clearConsole();
-    for (int i = 0; i < thread_cnt; i++) {
-        print_head(i, threads[i]);
-        std::cout << "Time: " << durations[i].count() << '\n';
+    std::cout << std::setw(NUM_WIDTH) << "#";
+    std::cout << std::setw(ID_WIDTH) << "Id";
+    std::cout << std::setw(NORMALIZED_PROGRESS_CNT) << "Progress Bar";
+    std::cout << std::setw(TIME_WIDTH) << "Time" << std::endl;
+
+    {
+        std::lock_guard<std::mutex> lock(write_lock);
+        for (int i = 0; i < thread_cnt; i++) {
+
+            std::cout << std::setw(NUM_WIDTH) << i;
+
+            threads.emplace_back(work, i + 1, div + (i < mod?1:0));
+            std::cout << std::setw(ID_WIDTH) << threads.back().get_id() << std::endl;
+        }
     }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    setNextLine(thread_cnt);
+
     return 0;
 }
